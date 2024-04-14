d3c3a65a54436b6817c654e539e55430b7bcb4f3
LeonardoBlanca <lssilvatati@gmail.com>
2024-04-14 04:19:29 -0300

    feat: Add a Get Requisition
    - Add requisition for GET method.
    - Add validation and error handling using Yup.
    - Add a route for Get requisition.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,13 +1,33 @@
-import bcrypt from "bcrypt"
-import { createUser } from "../repositories/user.repository";
+import bcrypt from "bcrypt";
+import { userValidation } from "../validations/user.validation";
+import { createUser, getAll } from "../repositories/user.repository";
 
 export const create = async (req, res) => {
+  try {
+    await userValidation.validate(req.body, { abortEarly: false })   
+
+    const hashPassword = await bcrypt.hash(req.body.password, 10);
+    req.body.password = hashPassword;
+
+    const user = await createUser(req.body);
+    res.status(200).send(user);
+  } catch (error) {
+    if (error.name === 'ValidationError') {
+        res.status(400).json({ errors: error.errors }); 
+      } else if (error.name === 'PrismaClientValidationError') {
+        res.status(400).json({ error: 'Database error' }); // Simple example
+      } else {
+        console.error("Unexpected Error:", error); 
+        res.status(500).send("Something went wrong");
+      }
+  }
+};
+
+export const get = async (req, res) => {
     try {
-        const hashPassword = await bcrypt.hash(req.body.password, 10);
-        req.body.password = hashPassword;
-        const user = await createUser(req.body);
-        res.status(200).send(user)
+        const users = await getAll();
+        res.status(200).send(users);
     } catch (error) {
-        res.status(400).send(error)
+        res.status(400).send(error);
     }
 }
--- src/repositories/user.repository.js
+++ src/repositories/user.repository.js
@@ -1,8 +1,33 @@
 import { prisma } from "../services/prisma";
 
 export const createUser = async (data) => {
-    const user = await prisma.user.create({
-        data,
-    });
-    return user;
-}
+  console.log("CreateUser in User Repository Works");
+  const user = await prisma.user.create({
+    data,
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      password: false,
+      phone: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+  });
+  return user;
+};
+
+export const getAll = async () => {
+  const users = await prisma.user.findMany({
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      password: false,
+      phone: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+  });
+  return users;
+};
